edb91e1b6e43c4a7b9eeb8428ddddbb2aedd214f
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-05 20:17:48 -0700

    toLowerCase convert all string to lowercase

--- toLowerCase.js
+++ toLowerCase.js
@@ -1,4 +1,5 @@
 //use lowercase method to all the characters in a string
 
 var myName = "My name is Farooq";
-console.log(myName.toLowerCase())
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
