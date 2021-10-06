4e114aad74ebca08e55248457e54646dc43ab3f1
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-05 20:25:12 -0700

    spilt method used to part of the given string

---
+++ spilt.js
@@ -0,0 +1,4 @@
+//spilt method use to part of a string and it converted to array
+
+var charUmar = "Umar Farooq is a great man. I like him and Follow him. I wish, I will meet with him in Jannah";
+console.log(charUmar.split(" "));
