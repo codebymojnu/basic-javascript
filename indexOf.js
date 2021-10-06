b4796f6d601f190fff30e43c198a22b26847920d
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-05 20:13:02 -0700

    use indexOf property to search a word or charecter in the string

---
+++ indexOf.js
@@ -0,0 +1,3 @@
+//use indexOf property to find the index of a word or charecter if not return -1
+var promise = "I will marry a beautiful girl on september 1, 2022";
+console.log(promise.indexOf('girl'));
