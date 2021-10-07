ac135459c7efe2e73d212bca7ec602577467f886
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-06 19:54:59 -0700

    push method used to add new item to the end of the array

---
+++ push.js
@@ -0,0 +1,5 @@
+//push method add new item to end of the array
+
+const brother = ["Nurul", "Milon", "Nirob", "Nannu", "Mofijul"];
+brother.push("Ashraful");
+console.log(brother); //output: ['Nurul', 'Milon', 'Nirob', 'Nannu', 'Mofijul', 'Ashraful']
