3eb1e05ae090441da20fae95e764c470729b89bc
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-08 07:07:44 +0600

    concat() - used to merge two or more arrays and returns a new array without changing the existing array

---
+++ concat().js
@@ -0,0 +1,7 @@
+//this method is used to merge two or more arrays and returns a new array without changing the existing array
+const oldStudent = ["Timer", "Rabby", "Shamim", "Jahidul"];
+const newStudent = ["Mehedi", "Mominul", "Al Mahmud"];
+const total = oldStudent.concat(newStudent);
+console.log(total)
+console.log(oldStudent)
+console.log(newStudent)
