fb00f6cd7ca929fc390f76a9860958ad693284f3
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-08 18:59:49 +0600

    every() method checks every element in the array return true or false through condition

---
+++ every().js
@@ -0,0 +1,3 @@
+const array = ["Asia", "Asma", "Reshma", "Priyanka", "mou"];
+const allNameLengthGreaterThan10 = array.every(element => element.length>2);
+console.log(allNameLengthGreaterThan10) //false
